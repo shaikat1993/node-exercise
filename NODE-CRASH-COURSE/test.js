c8905e862e253391d5edcc73984c8bc0db75be0e
shaikat1993 <mdsadidurrahman74@gmail.com>
2024-08-19 18:33:07 +0300

    the basic variable declaration and print for node.js

---
+++ NODE-CRASH-COURSE/test.js
@@ -0,0 +1,21 @@
+/*
+Node.js can be used to run JavaScript files on your computer. 
+You can use the node command to execute a JavaScript file, 
+like node test.js.
+*/
+
+// const userName = 'sadidur';
+// console.log(userName);
+
+// now we will create a function that will take a name and it will print with the name
+const greetings = (username) => {
+    console.log(`Hello ${username}`);
+}
+
+// calling the function for print out
+greetings('shayla');
+greetings('shaikat');
+
+
+
+
