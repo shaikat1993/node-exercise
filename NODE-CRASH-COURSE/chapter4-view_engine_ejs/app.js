e5caf917b64ea62dba17b88710d8b05a9da96ccf
shaikat1993 <mdsadidurrahman74@gmail.com>
2024-08-23 14:18:37 +0300

    Middleware is code that runs on the server between receiving a request and sending a response.
    1. how to use middleware in ejs view engines
    2. when we traverse the function we need to use next() middleware function to tell the system to go to the next function

--- NODE-CRASH-COURSE/chapter4-view_engine_ejs/app.js
+++ NODE-CRASH-COURSE/chapter4-view_engine_ejs/app.js
@@ -22,6 +22,15 @@ app.set('view engine', 'ejs');
 //listen for request
 app.listen(3000);
 
+// implementing middle ware
+app.use((req,res, next) => {
+    console.log('new request made');
+    console.log('host', req.hostname);
+    console.log('path', req.path);
+    console.log('method', req.method);
+    next();
+});
+
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
