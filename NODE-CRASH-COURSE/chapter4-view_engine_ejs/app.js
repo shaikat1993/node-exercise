9b14569129713e617fc3a75303ef3326bef3a218
shaikat1993 <mdsadidurrahman74@gmail.com>
2024-08-23 03:32:11 +0300

    Now we will focus on how to pass data using ejs to the dynamic view and update it asynchronously
    1.  we can pass a single value in ejs to show it one web page
    2. we can pass array or object too to directly show it in the web page
    
    So, The Whole scenario is like
    
    EJS Templates are processed through the EJS View engine on the server

--- NODE-CRASH-COURSE/chapter4-view_engine_ejs/app.js
+++ NODE-CRASH-COURSE/chapter4-view_engine_ejs/app.js
@@ -23,19 +23,24 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', {title: 'Home', blogs});
 })
 app.get('/about', (req,res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 })
 
 // now create an handler funtion for creating new blogs and show it into the web page
 app.get('/blogs/create' , (req,res) => {
-    res.render('createBlog');
+    res.render('createBlog', {title: 'Create a new Blog'} );
 })
 
 app.use((req,res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {title: '404'});
 })
 
 
